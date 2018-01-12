18029f48ee243db5e060e4c46b255794ea86b041
Samizu <asajiang@163.com>
2018-01-12 11:26:33 +0800

    依赖spring-boot-redis

--- drill-common/src/main/java/com/cmbchina/drill/common/dto/DivAreaDto.java
+++ drill-common/src/main/java/com/cmbchina/drill/common/dto/DivAreaDto.java
@@ -1,12 +1,13 @@
 package com.cmbchina.drill.common.dto;
 
+import java.io.Serializable;
 import lombok.Data;
 
 /**
  * Created by sanshui on 2018/1/10.
  */
 @Data
-public class DivAreaDto {
+public class DivAreaDto implements Serializable{
 
     private String vocabulary;
 
--- drill-producer/src/main/java/com/cmbchina/drill/producer/helper/CityAreaInit.java
+++ drill-producer/src/main/java/com/cmbchina/drill/producer/helper/CityAreaInit.java
@@ -11,6 +11,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,20 +19,27 @@ public class CityAreaInit {
 
     private static Logger logger = LoggerFactory.getLogger(CityAreaInit.class);
 
+    private static String REDIS_KEY_CITY_AREA = "drillCityArea";
+
     @Resource
     private CityAreaInfoPoMapper cityAreaInfoPoMapper;
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
     /**
      * 初始化城市地区列表
-     * @throws IOException
      */
     @PostConstruct
     public void init() throws IOException {
         String url = "http://www.dianping.com/citylistguonei?redir=aHR0cDovL3d3dy5kaWFucGluZy5jb20v";
         List<DivAreaDto> divAreaDtoList = CityReptile.searchCity(url);
         //持久化
+        redisTemplate.delete(REDIS_KEY_CITY_AREA);
+        redisTemplate.opsForValue().set(REDIS_KEY_CITY_AREA, divAreaDtoList);
         cityAreaInfoPoMapper.deleteByExample(new CityAreaInfoPoExample());
         cityAreaInfoPoMapper.insertBatch(CommonConvert.areaConvertCity.apply(divAreaDtoList));
         logger.info("地区数据持久化结束");
+
     }
 }
